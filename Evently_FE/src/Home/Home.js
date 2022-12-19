4ed3f87f1909db1a5d6239e7a1f20e48e09d517a
Tanay Joshi <Tanay@PCIND05.NEXION.LAN>
2022-12-19 11:19:56 +0530

    Made some UI changes in Home Page

--- Evently_FE/src/Home/Home.js
+++ Evently_FE/src/Home/Home.js
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import classes from "./Home.module.css";
 import Navbar from "../Navbar/Navbar";
+import EventModal from "../EventModal/EventModal";
 
 function Home(props) {
   const [events, setEvents] = useState([]);
-  console.log(events)
+  const [showEventModal, setShowEventModal] = useState(false);
+
   useEffect(() => {
     axios
       .get("events/all")
@@ -17,24 +19,48 @@ function Home(props) {
         console.log(e);
       });
   }, []);
+
+  const getEvent = (event) => {
+    let eventId = event.target.id;
+    axios.get(`/events/event/${eventId}`)
+    .then(res=>{
+      console.log(res)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  };
+
   return (
     <div>
+      {showEventModal && <EventModal setShowEventModal={setShowEventModal} />}
       <Navbar username={props.username} />
       <div className={classes.homePageContainer}>
         <div className={classes.eventsContainer}>
           {events.length
             ? events.map((event) => {
-               return <div className={classes.event}>
-                  <h2>{event.title}</h2>
-                  <p>{event.location}</p>
-                  <p>{event?.time}</p>
-                  <p>{event.description}</p>
-                </div>;
+                return (
+                  <div key={event._id} className={classes.event}>
+                    <h2>{event.title}</h2>
+                    <p>{event._id}</p>
+
+                    <p>{event.location}</p>
+                    <p>{event?.time}</p>
+                    <p>{event.description}</p>
+                    <button id={event._id} onClick={getEvent} className={classes.detailsBtn}>More Details</button>
+                  </div>
+                );
               })
             : null}
         </div>
         <div className={classes.sideContainer}>
-          <button>Create Event</button>
+          <button
+            onClick={() => {
+              setShowEventModal(true);
+            }}
+          >
+            Create Event
+          </button>
         </div>
       </div>
     </div>
