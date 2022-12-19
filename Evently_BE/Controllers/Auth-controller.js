55013e1e8252c18c6d4ff652ceeb59b2d1905f96
Tanay Joshi <Tanay@PCIND05.NEXION.LAN>
2022-12-19 11:10:10 +0530

    Updated Auth Logic: Added Bcrypt and added email verification on signup

--- Evently_BE/Controllers/Auth-controller.js
+++ Evently_BE/Controllers/Auth-controller.js
@@ -1,35 +1,47 @@
 const express = require("express");
 const User = require("../Models/User");
-
+const Tokens = require("../Models/Token");
+const sendMail = require("../Services/Mail-Service");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+const limiter = require("../Services/Rate-Limiter");
 const authRouter = express.Router();
 
-authRouter.post("/login", async (req, res, next) => {
-  console.log("Received login request", req.body);
-  const user = req.body;
-  let error_message = "";
-  const userFound = await User.findOne({ username: user.username });
+authRouter.post("/login", limiter, async (req, res, next) => {
+  try {
+    console.log("Received login request", req.body);
+    const user = req.body;
+    let error_message = "";
+    const userFound = await User.findOne({ username: user.username });
+    if (!userFound || user.password !== userFound.password) {
+      error_message = "Wrong username and/or password";
+    }
 
-  if (!userFound || user.password !== userFound.password) {
-    error_message = "Wrong username and/or password";
-  }
+    if (error_message !== "") {
+      return res.status(400).json({
+        error: {
+          error_message,
+          isAuth: false,
+        },
+      });
+    }
 
-  if (error_message !== "") {
-    return res.status(400).json({
+    req.session.isLoggedIn = true;
+    req.session.username = user.username;
+    res.status(200).json({
+      message: {
+        text: "Success",
+        isAuth: true,
+        username: user.username,
+      },
+    });
+  } catch (e) {
+    return res.status(500).json({
       error: {
-        error_message,
-        isAuth: false,
+        error_message: "Internal Server Error",
       },
     });
   }
-
-  req.session.isLoggedIn = true;
-  req.session.username = user.username;
-  res.status(200).json({
-    message: {
-      text: "Success",
-      isAuth: true,
-    },
-  });
 });
 
 authRouter.post("/signup", async (req, res, next) => {
@@ -39,11 +51,20 @@ authRouter.post("/signup", async (req, res, next) => {
   if (user.username.trim().length === 0) {
     formErrors.usernameError = "Username cannot be empty";
   }
+  if (user.username.trim().length < 3) {
+    formErrors.usernameError = "Username should be at least 3 characters";
+  }
+
+  if (/[^-_.a-zA-Z]/.test(user.username)) {
+    formErrors.usernameError =
+      "No special characters except hyphens, underscores and periods.";
+  }
+
   if (user.password.length < 6) {
     formErrors.passwordError = "Password must be at least 6 characters";
   }
   if (user.password.length === 0) {
-    formErrors.passwordError = "This field cannot be empty";
+    formErrors.passwordError = "Password cannot be empty";
   }
 
   const takenUsername = await User.findOne({ username: user.username });
@@ -56,26 +77,32 @@ authRouter.post("/signup", async (req, res, next) => {
     console.log(formErrors);
 
     return res.status(400).json({
-      message: {
+      error: {
         usernameError: formErrors.usernameError,
         passwordError: formErrors.passwordError,
       },
     });
   }
-  const newUser = new User({
+  const salt = await bcrypt.genSalt(12);
+  const hashedPassword = await bcrypt.hash(user.password, salt);
+
+  const newUser = await new User({
+    email: user.email,
     username: user.username,
-    password: user.password,
-  });
+    password: hashedPassword,
+  }).save();
 
-  newUser.save((err, user) => {
-    if (err) {
-      return res.status(500).json({
-        error: "Internal Server Error",
-      });
-    }
-  });
+  const token = await new Tokens({
+    userId: newUser._id,
+    token: crypto.randomBytes(32).toString("hex"),
+  }).save();
 
-  res.json({ message: "Record created successfully" });
+  const url = `${process.env.BASE_URL}users/${newUser._id}/verify/${token.token}`;
+  await sendMail(user.email, "Verify Email", url);
+
+  res.status(201).json({
+    message: "Verification Link is sent to your inbox",
+  });
 });
 
 authRouter.post("/logout", (req, res, next) => {
@@ -85,5 +112,35 @@ authRouter.post("/logout", (req, res, next) => {
 
   res.json({ message: "Logged out" });
 });
+
+authRouter.get(
+  `${process.env.BASE_URL}users/:user_id/verify/:token`,
+  async (req, res, next) => {
+    try {
+      const user = await User.findOne({ _id: req.params.user_id });
+      if (!user) {
+        return res.status(400).json({
+          error: "User not found",
+        });
+      }
+      const token = Tokens.findOne({
+        userId: req.params.user._id,
+        token: req.params.token,
+      });
+      if (!token) {
+        return res.status(400).json({ error: "Invalid Token/Link" });
+      }
+      await User.updateOne({ _id: user._id, verified: true });
+      await token.remove();
+
+      res.status(201).json({
+        message: "Email Verified",
+      });
+    } catch (e) {
+      res.status(500).json({ error: "Internal Server Error" });
+      console.log(e);
+    }
+  }
+);
 
 module.exports = authRouter;
