c494de1d8644e92771aebed584160d573683e086
Tanay Joshi <t.joshi@nexionindia.com>
2023-02-18 17:53:55 +0530

    Pulled changes from master

---
+++ Evently_FE/src/Navbar/Dropdown.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import LogoutIcon from "../Assets/logout.svg";
+import UserIcon from "../Assets/user.svg";
+import classes from "./Dropdown.module.css";
+
+function Dropdown(props) {
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    axios
+      .post("/logout")
+      .then((res) => {
+        props.setIsAuth(false);
+        navigate("/");
+      })
+      .catch((e) => {
+        console.log(e);
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <div className={classes.dropdownContainer}>
+      <div
+        className={classes.profileLinkCotaniner}
+        onClick={() => navigate(`/account/${props.username}`)}
+      >
+        <img src={UserIcon} alt="" />
+        <Link
+          className={classes.profileLink}
+          to={`/account/:${props.username}`}
+        >
+          Profile
+        </Link>
+      </div>
+      <div>
+        <button onClick={logoutHandler}>Logout</button>
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- Evently_FE/src/Navbar/Navbar.js
+++ Evently_FE/src/Navbar/Navbar.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Dropdown from "./Dropdown";
 import classes from "./Navbar.module.css";
 
 function Navbar(props) {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+  const navigate = useNavigate();
+
   return (
     <header>
       <nav>
         <ul className={classes.navbarContainer}>
-          <p>Evently</p>
+          <p onClick={() => navigate("/")}>Evently</p>
           <div>
             <li>MY EVENTS</li>
             <li>INVITES</li>
           </div>
-          <p>{props.username}</p>
+          <div
+            className={classes.menuCotainer}
+            onClick={() => setToggleDropdown(!toggleDropdown)}
+          >
+            {props.username}▾
+            {toggleDropdown ? (
+              <Dropdown username={props.username} setIsAuth={props.setIsAuth} />
+            ) : null}
+          </div>
         </ul>
       </nav>
     </header>
